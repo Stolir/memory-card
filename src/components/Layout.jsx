344c4cb77d7b4e3b2cc657e7eeb5e28b88f68808
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-08-20 13:33:51 +0300

    Add DifficultyScreen to layout and change screen controlling state

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,28 @@
 import "../styles/Layout.css"
 import { useState } from "react"
 import StartScreen from "./StartScreen"
+import DifficultyScreen from "./DifficultyScreen"
 
 function Layout() {
-  const [isStarted, setIsStarted] = useState(false)
+
+  const screens = ['start', 'difficulty', 'game']
+  const [currentScreen, setCurrentScreen] = useState(0)
+
   const handleStart = () => {
-    console.log("clicked")
-    setIsStarted(!isStarted)
+    setCurrentScreen(currentScreen + 1)
   }
+
+  const [cardCount, setCardCount] = useState(0)
 
   return (
     <>
       <StartScreen 
         handleClick={handleStart}
-        currentScreen={isStarted ? false : true}
+        currentScreen={screens[currentScreen] === 'start'}
+      />
+      <DifficultyScreen
+        handleClick={handleStart} 
+        currentScreen={screens[currentScreen] === 'difficulty'}
       />
     </>
   )
