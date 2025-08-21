79e8ad469a1b564070af4f625a15a35ed3ccd72f
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-08-22 02:41:16 +0300

    Write shuffleArray helper function

--- src/utils/helper.jsx
+++ src/utils/helper.jsx
@@ -1,9 +1,17 @@
 export function generateRandomNumberArray(length, max) {
   const uniqueNumbers = new Set();
   while (uniqueNumbers.size < length) {
-    uniqueNumbers.add(Math.floor(Math.random() * max))
+    uniqueNumbers.add(Math.floor(Math.random() * (max + 1)))
   }
 
   return Array.from(uniqueNumbers)
 }
 
+export function shuffleArray(array) {
+  const result = [...array];
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
+}
