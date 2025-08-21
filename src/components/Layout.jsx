6481c915e3e35e71d50e071f88072b2cabddb9bc
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-08-22 02:39:48 +0300

    Change how "current screen" is displayed
    Instead of rendering all screens and setting them to 'hidden' while
    another screen is being displayed, only the screen that is currently
    active is rendered.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,31 +7,39 @@ import GameScreen from "./GameScreen"
 function Layout() {
 
   const screens = ['start', 'difficulty', 'game']
-  const [currentScreen, setCurrentScreen] = useState(2)
+  const [currentScreen, setCurrentScreen] = useState(0)
 
   const handleStart = () => {
     setCurrentScreen(currentScreen + 1)
   }
 
-  const [cardCount, setCardCount] = useState(12)
+  const [cardCount, setCardCount] = useState(0)
 
-  return (
-    <>
+  if (screens[currentScreen] === 'start') {
+    return (
       <StartScreen 
         handleClick={handleStart}
-        currentScreen={screens[currentScreen] === 'start'}
       />
+    )
+  }
+  
+  if (screens[currentScreen] === 'difficulty') {
+    return (
       <DifficultyScreen
         handleStart={handleStart}
         handleSetDifficulty={setCardCount}
-        currentScreen={screens[currentScreen] === 'difficulty'}
       />
+    )
+  }
+
+  if (screens[currentScreen] === 'game') {
+    return (
       <GameScreen 
         cardCount={cardCount}
-        currentScreen={screens[currentScreen] === 'game'}
+        handleScreenSelection={setCurrentScreen}
       />
-    </>
-  )
+    )
+  }
 }
 
 export default Layout
